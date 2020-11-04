92f9bcc96a97dc29c2025f0d6e070bb61bf5467b
biqqles <biqqles@protonmail.com>
2020-11-04 19:21:12 +0000

    Init: show a warning if process is inaccessible

--- src/wingman/main.py
+++ src/wingman/main.py
@@ -18,6 +18,8 @@ along with Wingman.  If not, see <http://www.gnu.org/licenses/>.
 
 This file contains the application's entry point - main().
 """
+from PyQt5 import QtWidgets
+
 import os
 
 import flint as fl
@@ -27,21 +29,32 @@ from wingman.windows.main.layout import MainWindow
 from wingman.windows.boxes import configuration, loading
 
 
-def main():
+def main() -> int:
     """Main application entry point."""
     if not fl.paths.is_probably_freelancer(config.paths['freelancer_dir']):
         configuration.ConfigurePaths(mandatory=True).exec()
+
     fl.paths.set_install_path(config.paths['freelancer_dir'])
+
     if IS_WIN:
         import flair
-        flair.set_install_path(config.paths['freelancer_dir'])
+        try:
+            flair.set_install_path(config.paths['freelancer_dir'])
+        except PermissionError:
+            # If flair is denied access, show a warning and quit. Unfortunately there is no good way to do this if
+            # Freelancer is opened while the application is open, and it will fail silently. This is better than
+            # nothing, though
+            QtWidgets.QMessageBox(QtWidgets.QMessageBox.Critical, 'Access denied',
+                                  "Wingman could not access Freelancer's process. "
+                                  "Check that Freelancer is not running as administrator and try again.").exec()
+            return 1
 
     loading.LoadingFiles().exec()
 
     mainWindow = MainWindow()
 
-    os._exit(app.exec())  # bad! but sys.exit isn't working for some reason (not to do with excepthook)
+    return app.exec()
 
 
 if __name__ == '__main__':
-    main()
+    os._exit(main())  # bad! but sys.exit isn't working for some reason (not to do with excepthook)
